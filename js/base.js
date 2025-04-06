5211606d273265b6eb4298d02acca750fbacc438
Martin <martinmiadosqui@gmail.com>
2025-04-06 15:41:26 -0300

    arreglo fecha actual

--- js/base.js
+++ js/base.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Bootstrap cargado en todas las páginas");
 });
+
+function anioActual() {
+    let anioActual = new Date().getFullYear();
+    return anioActual;
+  }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    let añoActual = document.getElementById("añoActual").innerText ="Copyright © " + anioActual() + " Cereceda & Asociados.";
+  });
